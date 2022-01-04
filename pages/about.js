aae6dfcdde4368c4d53cddd3797a763b433a194f
Pedro Martins <pedrovrima@gmail.com>
2022-01-04 14:38:30 -0300

    about page

--- pages/about.js
+++ pages/about.js
@@ -18,9 +18,13 @@ export default function AboutPage() {
         <section>
           <h2 className="mb-3 text-xl font-bold">What is Next.js?</h2>
           <p>
-            Next.js is a minimalistic framework for creating server-rendered
-            React applications.
-          </p>
+My name is Pedro Martins, I am a software developer currently using JavaScript to build solutions to my day-to-day problems and for the organizations I work with.          </p>
+<p>Most of the solutions I build involve both a back-end, to serve and store data, and a front-end, for the users to interact with. Firstly I used a combination of Express and "pure" React for the projects, but I am migrating towards using Next.js. </p>
+
+<p>Previous Carreer and Achievements</p>
+
+<p>I am graduated in Biology with a Master's Degree in Ecology. During my career, I worked at two different fronts: fieldwork and data analysis and management.</p>
+<p>From 2014 to 2019 I have worked as a bird bander, most of the time managing teams of 2 or more biologists. Even though this work is not easily realatablw with tech work, it thought me how to work and lead a team under pressure and in stressful situations. I also had the opportunity to participate and teach </p>
         </section>
       </div>
 
