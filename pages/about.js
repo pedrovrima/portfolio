1034b64704a7a37ef5aa62a0350d2e1f63dad370
Pedro Martins <pedrovrima@gmail.com>
2022-01-25 16:50:50 -0300

    up to date

--- pages/about.js
+++ pages/about.js
@@ -2,38 +2,50 @@ import Image from "next/image";
 
 export default function AboutPage() {
   return (
-    <div className="grid md:grid-cols-2 gap-6 grid-cols-1">
-      <div>
-        <section className="mb-6">
-          <h2 className="mb-3 text-xl font-bold">What is Tailwind?</h2>
+    <div className="mx-12">
+      <p>
+        My name is Pedro Martins, I am a software developer currently using
+        JavaScript to build solutions to my day-to-day problems and for the
+        organizations I work with.{" "}
+      </p>
+      <p>
+        Most of the solutions I build involve both a back-end, to serve and
+        store data, and a front-end, for the users to interact with.
+      </p>
 
-          <p>
-            Tailwind CSS is a highly customizable, low-level CSS framework that
-            gives you all of the building blocks you need to build bespoke
-            designs without any annoying opinionated styles you have to fight to
-            override.
-          </p>
-        </section>
+      <p>
+        Among my top skills, mostly gained during my career as a ecologist, are:
+      </p>
+      <ul className="list-disc ml-8">
+      <li >Team work</li>
+      <li>Leadership</li>
+      <li>Problem solving</li>
+      <li>Working under stress</li>
+      <li>Decision making</li>
+      </ul>
+      <p>
+        Firstly I used a combination of Express and "pure" React for the
+        projects, but I am migrating towards using Next.js.{" "}
+      </p>
 
-        <section>
-          <h2 className="mb-3 text-xl font-bold">What is Next.js?</h2>
-          <p>
-My name is Pedro Martins, I am a software developer currently using JavaScript to build solutions to my day-to-day problems and for the organizations I work with.          </p>
-<p>Most of the solutions I build involve both a back-end, to serve and store data, and a front-end, for the users to interact with. Firstly I used a combination of Express and "pure" React for the projects, but I am migrating towards using Next.js. </p>
+      <p>Previous Carreer and Achievements</p>
 
-<p>Previous Carreer and Achievements</p>
-
-<p>I am graduated in Biology with a Master's Degree in Ecology. During my career, I worked at two different fronts: fieldwork and data analysis and management.</p>
-<p>From 2014 to 2019 I have worked as a bird bander, most of the time managing teams of 2 or more biologists. Even though this work is not easily realatablw with tech work, it thought me how to work and lead a team under pressure and in stressful situations. I also had the opportunity to participate and teach </p>
-        </section>
-      </div>
-
-      <Image
-        alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
-        src="/critter.svg"
-        width={476}
-        height={297.17}
-      />
+      <p>
+        I am graduated in Biology with a Master's Degree in Ecology. During my
+        career, I worked at two different fronts: fieldwork and data analysis
+        and management.
+      </p>
+      <p>
+        From 2014 to 2019 I have worked as a bird bander, most of the time
+        managing teams of 2 or more biologists. Even though this work is not
+        easily realatablw with tech work, it thought me how to work and lead a
+        team under pressure and in stressful situations. I also had the
+        opportunity to participate and teach on several workshops.{" "}
+      </p>
+      <p>
+        During this period I spent most of my time performing data management
+        and data analysis tasks on R;
+      </p>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 import Image from "next/image";
 import Projects from "@components/projects";
+import { useRef, useEffect } from "react";
+
+const getPixelRatio = (context) => {
+  var backingStore =
+    context.backingStorePixelRatio ||
+    context.webkitBackingStorePixelRatio ||
+    context.mozBackingStorePixelRatio ||
+    context.msBackingStorePixelRatio ||
+    context.oBackingStorePixelRatio ||
+    context.backingStorePixelRatio ||
+    1;
+
+  return (window.devicePixelRatio || 1) / backingStore;
+};
 
 const proje = [
   {
@@ -85,7 +99,7 @@ const proje = [
     images: [
       "/projects/sono1.png",
       "/projects/sono2.png",
-      "/projects/sono3.png",
+      "/projects/sono3.png"
     ],
 
     techstack: {
@@ -94,8 +108,8 @@ const proje = [
         { name: "React" },
         { name: "Express" },
         { name: "MongoDB" },
-        { name: "Heroku" },
-      ],
+        { name: "Heroku" }
+      ]
     },
     title: "Baby sleep tracker",
     description:
@@ -104,17 +118,10 @@ const proje = [
     liveUrl: "www.oama.eco.br"
   },
   {
-    images: [
-      "/projects/sono1.png",
-      "/projects/sono2.png",
-      "/projects/sono3.png",
-    ],
+    images: ["/projects/pomodoro.png"],
 
     techstack: {
-      major: [
-        { name: "Firefox" },
-
-      ],
+      major: [{ name: "Firefox" }]
     },
     title: "Pomodoro Blocker",
     description:
@@ -125,6 +132,112 @@ const proje = [
 ];
 
 export default function IndexPage() {
+  const ref = useRef();
+
+  // useEffect(() => {
+  //   let canvas = ref.current;
+  //   let context = canvas.getContext('2d');
+  //   let requestId;
+  //   var stars = [], // Array that contains the stars
+  //     FPS = 45, // Frames per second
+  //     x = 2, // Number of stars
+  //     mouse = {
+  //       x: 0,
+  //       y: 0
+  //     }; // mouse location
+
+  //   // Push stars to array
+
+  //   for (var i = 0; i < x; i++) {
+  //     stars.push({
+  //       x: Math.random() * canvas.width,
+  //       y: Math.random() * canvas.height,
+  //       radius: Math.random() * 1 + 1,
+  //       vx: Math.floor(Math.random() * 50) - 25,
+  //       vy: Math.floor(Math.random() * 50) - 25
+  //     });
+  //   }
+
+  //   // Draw the scene
+
+  //   function draw() {
+  //     context.clearRect(0, 0, canvas.width, canvas.height);
+
+  //     context.globalCompositeOperation = "lighter";
+
+  //     for (var i = 0, x = stars.length; i < x; i++) {
+  //       var s = stars[i];
+
+  //       context.fillStyle = "#fff";
+  //       context.beginPath();
+  //       context.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+  //       context.fill();
+  //       context.fillStyle = "black";
+  //       context.stroke();
+  //     }
+
+  //     context.beginPath();
+  //     for (var i = 0, x = stars.length; i < x; i++) {
+  //       var starI = stars[i];
+  //       context.moveTo(starI.x, starI.y);
+  //       if (distance(mouse, starI) < 150) context.lineTo(mouse.x, mouse.y);
+  //       for (var j = 0, x = stars.length; j < x; j++) {
+  //         var starII = stars[j];
+  //         if (distance(starI, starII) < 150) {
+  //           //context.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+  //           context.lineTo(starII.x, starII.y);
+  //         }
+  //       }
+  //     }
+  //     context.lineWidth = 0.05;
+  //     context.strokeStyle = "white";
+  //     context.stroke();
+  //   }
+
+  //   function distance(point1, point2) {
+  //     var xs = 0;
+  //     var ys = 0;
+
+  //     xs = point2.x - point1.x;
+  //     xs = xs * xs;
+
+  //     ys = point2.y - point1.y;
+  //     ys = ys * ys;
+
+  //     return Math.sqrt(xs + ys);
+  //   }
+
+  //   // Update star locations
+
+  //   function update() {
+  //     for (var i = 0, x = stars.length; i < x; i++) {
+  //       var s = stars[i];
+
+  //       s.x += s.vx / FPS;
+  //       s.y += s.vy / FPS;
+
+  //       if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+  //       if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+  //     }
+  //   }
+
+  //   canvas.addEventListener("mousemove", function (e) {
+  //     mouse.x = e.clientX;
+  //     mouse.y = e.clientY;
+  //   });
+
+  //   // Update and draw
+
+  //   function tick() {
+  //     update();
+  //     requestId=requestAnimationFrame(tick);
+  //   }
+
+  //   tick();
+  //   return () => {
+  //     cancelAnimationFrame(requestId);
+  //   };
+  // });
   return (
     <div className="flex flex-col items-center justify-center space-y-2">
       <div className="w-full p-8 mb-12 py-36 bg-gray-900 ">
@@ -135,13 +248,19 @@ export default function IndexPage() {
           I am a Fullstack Developer that &#128154; finding simple solutions for
           complex problems
         </h2>
+        <canvas ref={ref} style={{position:"absolute"}}></canvas>
       </div>
       <div className="flex flex-col  items-center py-24 bg-gray-50">
         <h1 className="text-3xl mb-4 font-bold">About </h1>
-      <p className="px-8 sm:px-96 text-lg">I am a graduated ecologist who has spent most of my carrer doing data management and statistical analysis using R. In the last couple years I started transiotioning to software develepoment, aiming to create tools that facilitate the work done by the organizations I work with.</p>
+        <p className="px-8 sm:px-96 text-lg">
+          I am a graduated ecologist who has spent most of my carrer doing data
+          management and statistical analysis using R. In the last couple years
+          I started transiotioning to software develepoment, aiming to create
+          tools that facilitate the work done by the organizations I work with.
+        </p>
       </div>
       <div className=" mb-24">
-      <Projects projects={proje}></Projects>
+        <Projects projects={proje}></Projects>
       </div>
     </div>
   );
