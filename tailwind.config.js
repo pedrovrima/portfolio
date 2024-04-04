bb9f9187070a111f401698fe1094da2c6b3250f7
Falco femoralis <falco@Falcos-MacBook-Pro.local>
2024-04-04 10:44:15 -0300

    updating tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
